6008b5bce373d9b5bb9f6d6c8eb221fff3a4d5cb
gary go <supemeko@gmail.com>
2024-09-19 18:50:01 +0800

    fix(lex): fix the problem that forget consume b'=' when parse Token::Equal

--- crates/simple-parser/src/lex.rs
+++ crates/simple-parser/src/lex.rs
@@ -323,6 +323,7 @@ impl<R: Read> Lex<R> {
             }
             b'=' => {
                 if self.guess_byte(b'=')? {
+                    self.next()?;
                     Token::Equal
                 } else {
                     Token::Assign
